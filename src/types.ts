fd2987c494629a834349db98309418c1548f1ce2
valpr <me@andrewfong.ca>
2020-05-11 19:03:47 -0400

    created initial types-user/entry, enums, weights

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+export interface entry {
+    description: string;
+    date: number;
+    time: string;
+    calories: number;
+}
+
+
+interface weightEntry {
+    date: Date;
+    weight: number;   
+}
+
+
+enum Effort {
+    Sedentary = 1.2,
+    Light = 1.4,
+    Moderate = 1.5,
+    Heavy = 1.7,
+    Extreme = 1.8
+  }
+
+  enum Gender {
+    Male = 1,
+    Female = 0
+}
+
+enum Objective {
+    Gain = 1.3,
+    Neutral = 1.0,
+    Loss = 0.8
+  }
+
+export interface user {
+    firstname: string;
+    lastname: string;
+    entries: [string];
+    gender: Gender;
+    height: number;
+    weights: [weightEntry];
+    effort: Effort;
+    objective: Objective;
+}
